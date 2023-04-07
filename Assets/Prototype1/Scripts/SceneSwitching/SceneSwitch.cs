7f409e0d95d3c5ba72667ffbea7fbe4a4835e536
danieljharris <danieljharris26@yahoo.co.uk>
2023-04-07 17:18:27 +0100

    Fusion wise scene switching
    - Scene switching now retains the same NetworkRunner and players instead of making them new for each scene switch
    - NetworkRunnerHandler is now set to max instance of 1
    - NetworkPlayer has been added to DontDestroyOnLoad
    - Scene Selector panel back button now takes you to the settings panel
    - Removed FusionNetworkRunner tag as is no longer needed

--- Assets/Prototype1/Scripts/SceneSwitching/SceneSwitch.cs
+++ Assets/Prototype1/Scripts/SceneSwitching/SceneSwitch.cs
@@ -1,16 +1,7 @@
-using Fusion;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
-    public void LoadScene(int sceneIndex)
-    {
-        // Get the NetworkRunner and shut it down
-        NetworkRunner runner = GameObject.FindGameObjectWithTag("FusionNetworkRunner").GetComponent<NetworkRunner>();
-        runner.Shutdown();
-
-        // Load the next scene which creates a new NetworkRunner
-        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
-    }
+    public void LoadScene(int sceneIndex) => SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
 }
